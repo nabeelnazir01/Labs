0deeed6fb86ac37ed092fd89f2bbb640ceee2da7
Nabeel Nazir <nabeelnazir@pop-os.localdomain>
2020-04-01 10:54:47 -0400

    nabeelnazir01

--- mycalc/mycalc.sh
+++ mycalc/mycalc.sh
@@ -1,21 +1,63 @@
-first=$1
-second=$2
-third=$3
+#!/bin/bash
+
+#Lab 9
+# Nabeel Nazir
+# 040974403
+# Operating Systems Fund(GNU/Linux)
+# Section 300
+# Calculator for plus or minus. Can take in parameters also.
+
 user='p'
 
-if [ $# -eq 3 ]; then
-if [ $second == + ]; then
-	echo $(($first+$third))
-elif [ $second == - ]; then
-	echo $(($first-$third))
-fi
-else
+if [[ $# -eq 3 ]] && [[ $1 =~ ^[+-]?[0-9]+$ ]] && [[ $3 =~ ^[+-]?[0-9]+$ ]]; then
+	if [ $2 == + ]; then
+		echo The sum of $1 plus $3 equals $(($1+$3))
+	elif [ $2 == - ]; then
+		echo The sum of $1 minus $3 equals $(($1-$3))
+	else
+		echo The second parameter is not a + or a -!
+	fi
+elif [ $# -eq 0 ]; then
 	while [ ${user^^} != 'X' ]; do
+		clear
 		printf 'C) Calculation\nX) Exit\n'
 		read -p 'Enter a choice: ' user
 		if [ ${user^^} == 'C' ]; then
-			echo hi
+			read -p 'Please enter an integer number or X to exit: ' first
+			if [ ${first^^} == 'X' ]; then
+				clear
+				continue
+			fi
+			printf '+) Add\n-) Subtract\nX) Exit\n'
+			read second
+			if [ $second == + ] || [ $second == - ]; then
+				read -p 'Please enter another integer number or X to exit: ' third
+				if [ ${third^^} == 'X' ]; then
+					clear
+					continue
+				fi
+				echo Answer is $(($first$second$third))
+				sleep 3
+				clear
+				continue
+			elif [ ${user^^} != 'X' ]; then
+				echo wrong input!
+				clear
+				continue
+			else
+				clear
+				continue
+			fi
+		elif [ ${user^^} != 'X' ]; then
+			clear
+			echo wrong input!
+		else
+			clear
+			continue
 		fi
-	done	
+	done
+else
+	clear
+	echo Wrong parameters!	
 fi
 echo Bye!
